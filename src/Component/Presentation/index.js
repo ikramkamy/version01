824a9a8eef9e3efc8ce5eecd78608b58ca13b3e4
ikramkamy <ikramzerkout13@gmail.com>
2021-04-19 03:05:51 -0700

    "the folder build is created"

--- src/Component/Presentation/index.js
+++ src/Component/Presentation/index.js
@@ -9,7 +9,7 @@ function Presentation(props) {
     <div className="presentation" style={{ 
       backgroundImage: `url("/images/images 2_plan de travail 1.png")` }}>
 <h1>PRESENTATION DE L'ENTREPRISE </h1>
-<p>Comptempt plus de 21 ans d'éxpériences, Barcket Inox se spécialise dans la fabrication de cuves et d'équipements à base de matiére inoxydable déstinée pour la collect de lait et la fabrication fromagére</p>
+<p>Comptant plus de 21 ans d'éxpériences, Barcket Inox se spécialise dans la fabrication de cuves et d'équipements à base de matiére inoxydable déstinée pour la collect de lait et la fabrication fromagére</p>
 
 </div>
 
